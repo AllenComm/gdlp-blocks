66b6954bbd9ccb5ccfb30a25a6db524ee8931c1b
BennettDISH <43225774+BennettDISH@users.noreply.github.com>
2024-06-07 14:39:28 -0600

    imports

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 import LayeredImage from './src/layered-image.js';
+import AudioBlock from './src/audio-block.js';
+import CounterBlock from './src/counter-block.js';
+import ImageBlock from './src/image-block.js';
+import IntroBlock from './src/intro-block.js';
+import McQuestion from './src/mc-question.js';
+import SwapBlock from './src/swap-block.js';
+import SwapButtonsBlock from './src/swap-buttons-block.js';
+import SwapTextBlock from './src/swap-text-block.js';
+import VideoBlock from './src/video-block.js';
+
 
 export {
-    LayeredImage
+    LayeredImage,
+    AudioBlock,
+    CounterBlock,
+    ImageBlock,
+    IntroBlock,
+    McQuestion,
+    SwapBlock,
+    SwapButtonsBlock,
+    SwapTextBlock,
+    VideoBlock
 }
